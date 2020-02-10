9dc8169eef01168a9a46a193ab3a66929543d3e6
Sunny Golovine <sunny@glvn.co>
2020-02-10 12:59:35 -0500

    Add profile, readme, and some other stuff

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-
-// Bootstrap theme
-// import "bootswatch/dist/flatly/bootstrap.min.css"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./src/styles/global.css"
-import "./src/styles/bootstrap-overrides.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './src/styles/global.css'
+import './src/styles/bootstrap-overrides.css'
