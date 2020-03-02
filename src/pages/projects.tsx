02652717a02e24a4418ec798b6f651d3f20fbfdb
Sunny Golovine <sunny@glvn.co>
2020-03-02 11:43:56 -0500

    Modified project page to run off data

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,30 +1,22 @@
 import React from 'react'
-import Text from '../components/Text'
 import { ProjectCard } from '../components/ProjectCard'
+import { projects } from '../data/projects'
+
 import styled from 'styled-components'
 
 export default () => {
   return (
     <ProjectContainer>
-      <Text>Hello World /projects</Text>
-      <ProjectCard
-        name="fav.sh"
-        desc="Open source bookmark manager that backs up to Github Gist"
-        // webUrl="#"
-        githubUrl="#"
-      />
-      <ProjectCard
-        name="fav.sh"
-        desc="Open source bookmark manager that backs up to Github Gist"
-        webUrl="#"
-        // githubUrl="#"
-      />
-      <ProjectCard
-        name="fav.sh"
-        desc="Open source bookmark manager that backs up to Github Gist"
-        // webUrl="#"
-        // githubUrl="#"
-      />
+      {projects.map((project) => {
+        return (
+          <ProjectCard
+            name={project.name}
+            desc={project.desc}
+            webUrl={project.webUrl}
+            githubUrl={project.gitUrl}
+          />
+        )
+      })}
     </ProjectContainer>
   )
 }
