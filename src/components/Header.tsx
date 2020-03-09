0f6c24413009f31b26652799022e36b0878667f2
Sunny Golovine <sunny@glvn.co>
2020-03-09 17:33:41 -0400

    Remove HeaderImage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import { HeaderImage } from './HeaderImage'
 
 const links = [
   {
@@ -18,8 +17,8 @@ const links = [
 export const Header = () => {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg">
-        <HeaderImage />
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <NavLink to="/">Sunny Golovine</NavLink>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse
           id="responsive-navbar-nav"
@@ -40,15 +39,18 @@ export const Header = () => {
   )
 }
 
-const NavLink = styled(Link)`
+export const NavLink = styled(Link)`
   font-size: 18px;
-  border-bottom: 3px solid;
   padding-top: 5px;
   padding-bottom: 5px;
   margin: 15px;
   text-decoration: none;
-  border-bottom-color: transparent;
-  width: 120px;
   text-align: center;
   text-transform: uppercase;
+
+  color: #fff;
+  :hover {
+    color: grey;
+    text-decoration: none;
+  }
 `
