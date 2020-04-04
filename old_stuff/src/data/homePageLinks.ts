5fa6b9e683f22402be8d9c9ec8a84cb7807cff54
Sunny Golovine <sunny@glvn.co>
2020-04-04 19:35:13 -0400

    Remove old stuff

--- old_stuff/src/data/homePageLinks.ts
+++
@@ -1,8 +0,0 @@
-export const pageLinks = {
-  hometown: 'https://en.wikipedia.org/wiki/Yekaterinburg',
-  atlanta: 'https://en.wikipedia.org/wiki/Atlanta',
-  react: 'https://reactjs.org/',
-  reactNative: 'https://reactnative.dev/',
-  gatsby: 'https://www.gatsbyjs.org/',
-  more: 'https://github.com/sgolovine',
-}
--- old_stuff/src/pages/resume.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const resume = require('../docs/resume.docx')
-
-export default () => <a href={resume}>Download Resume</a>
