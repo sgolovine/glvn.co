b58a01b59c7a70fc4450102cb2ca3f61f21ad8c1
Sunny Golovine <sunny@glvn.co>
2020-04-05 12:16:11 -0400

    Finished linting JS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import MainLayout from '~/layouts/MainLayout'
+import { AppProps } from 'next/app'
 
 import '~/styles/index.scss'
 
-export default function App({
-  Component,
-  pageProps,
-}: {
-  Component: any
-  pageProps: any
-}) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <MainLayout>
       <Component {...pageProps} />
