4b48892c970ceccb8fe1ee9beab0c157e974a09f
Sunny Golovine <sunny@glvn.co>
2020-04-05 13:43:04 -0400

    Style contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,16 +2,24 @@ import React from 'react'
 
 export default () => {
   return (
-    <div className="contact-form-container">
-      <label htmlFor="contact-name">Name</label>
-      <input className="contact-form-input" id="contact-name" />
-      <label htmlFor="contact-email">Email</label>
-      <input className="contact-form-input" id="contact-email" />
-      <label htmlFor="contact-message">Message</label>
-      <textarea className="contact-form-textarea" id="contact-message" />
-      <div className="contact-form-button-container">
-        <button className="form-button">Clear</button>
-        <button className="form-button">Submit</button>
+    <div className="layout-centered">
+      <div className="contact-form-container">
+        <label className="contact-form-label" htmlFor="contact-name">
+          Name
+        </label>
+        <input className="contact-form-input" id="contact-name" />
+        <label className="contact-form-label" htmlFor="contact-email">
+          Email
+        </label>
+        <input className="contact-form-input" id="contact-email" />
+        <label className="contact-form-label" htmlFor="contact-message">
+          Message
+        </label>
+        <textarea className="contact-form-textarea" id="contact-message" />
+        <div className="contact-form-button-container">
+          <button className="contact-form-action-button">Clear</button>
+          <button className="contact-form-action-button">Submit</button>
+        </div>
       </div>
     </div>
   )
