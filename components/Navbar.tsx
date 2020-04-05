db855973590a05083c2f16c202a344c1de6a8151
Sunny Golovine <sunny@glvn.co>
2020-04-04 23:27:31 -0400

    Remove transparent stuff

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,11 +7,9 @@ const LinkItem = ({ name, path }: { name: string; path: string }) => {
   const { pathname } = useRouter()
   const re = new RegExp(path)
   const active = re.test(pathname)
-  const isHomePage = pathname === '/'
 
   const styles = classNames('navbar-navitem-text', {
     'navbar-navitem-text-active': active,
-    'navbar-navitem-transparent': isHomePage,
   })
 
   return (
@@ -24,28 +22,12 @@ const LinkItem = ({ name, path }: { name: string; path: string }) => {
 }
 
 export const Navbar = () => {
-  const { pathname } = useRouter()
-
-  const isHomePage = pathname === '/'
-
-  const styles = classNames(
-    'navbar-container',
-    { 'navbar-container-light': !isHomePage },
-    { 'navbar-container-transparent': isHomePage }
-  )
-
-  const navbarBrandStyles = classNames('navbar-brand', {
-    'navbar-brand-transparent': isHomePage,
-  })
-
-  console.log('styles', styles)
   return (
-    <div className={styles}>
+    <div className="navbar-container">
       <Link href="/">
-        <a className={navbarBrandStyles}>Sunny Golovine</a>
+        <a className="navbar-brand">Sunny Golovine</a>
       </Link>
       <div className="navbar-links">
-        <LinkItem name="About" path="/about" />
         <LinkItem name="Projects" path="/projects" />
         <LinkItem name="Contact" path="/contact" />
         <LinkItem name="Resume" path="/resume" />
