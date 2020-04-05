c4409129fa8ccbe6fa78fc8512b4be6debc74c33
Sunny Golovine <sunny@glvn.co>
2020-04-04 20:15:18 -0400

    More SASS work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import MainLayout from '~/layouts/MainLayout'
 
 import '~/styles/_reset.css'
-import '~/styles/root.css'
+import '~/styles/root.scss'
 import '~/styles/layout.css'
 
 import '~/styles/linker.css'
-import '~/styles/navbar.css'
+import '~/styles/navbar.scss'
 
 export default function App({
   Component,
