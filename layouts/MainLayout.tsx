0e66119572204518896a9258af712fcf007ef14f
Sunny Golovine <sunny@glvn.co>
2020-04-05 14:52:07 -0400

    Fix max width styling for contact page

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,11 +1,23 @@
 import React, { ReactNode } from 'react'
 import { Navbar } from '~/components/Navbar'
+import { useRouter } from 'next/router'
+import classNames from 'classnames'
 
-export default ({ children }: { children: ReactNode }) => (
-  <div className="main-layout-container">
-    <div className="main-layout-header-container">
-      <Navbar />
+export default ({ children }: { children: ReactNode }) => {
+  const { pathname } = useRouter()
+  const re = new RegExp('/contact')
+  const isContactPage = re.test(pathname)
+
+  const styles = classNames({
+    'main-layout-content-container': !isContactPage,
+    'main-layout-content-container-centered': isContactPage,
+  })
+  return (
+    <div className="main-layout-container">
+      <div className="main-layout-header-container">
+        <Navbar />
+      </div>
+      <div className={styles}>{children}</div>
     </div>
-    <div className="main-layout-content-container">{children}</div>
-  </div>
-)
+  )
+}
