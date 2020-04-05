3ff9f17a4d87aaeba16d22452a3479486fa21542
Sunny Golovine <sunny@glvn.co>
2020-04-04 21:28:38 -0400

    Wip

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-export default () => (
-  <div className="page">
-    <p className="heading">Hello world from Next.js</p>
-    <p className="heading">Your are here: /contact</p>
-  </div>
-)
+export default () => {
+  return (
+    <div className="page">
+      <div className="contact-form-container">
+        <label htmlFor="contact-name">Name</label>
+        <input className="contact-form-input" id="contact-name" />
+        <label htmlFor="contact-email">Email</label>
+        <input className="contact-form-input" id="contact-email" />
+        <label htmlFor="contact-message">Message</label>
+        <textarea className="contact-form-textarea" id="contact-message" />
+        <div className="contact-form-button-container">
+          <button className="form-button">Clear</button>
+          <button className="form-button">Submit</button>
+        </div>
+      </div>
+    </div>
+  )
+}
